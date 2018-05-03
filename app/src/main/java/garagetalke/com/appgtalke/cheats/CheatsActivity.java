208d23939341d40d7106e98e56e6858f60aff29a
cristiano_gaspar <cristiano_gaspar@terceiros.sicredi.com.br>
2018-05-02 22:48:23 -0300

    Add Activity Cheats

---
+++ app/src/main/java/garagetalke/com/appgtalke/cheats/CheatsActivity.java
@@ -0,0 +1,15 @@
+package garagetalke.com.appgtalke.cheats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import garagetalke.com.appgtalke.R;
+
+public class CheatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheats);
+    }
+}
