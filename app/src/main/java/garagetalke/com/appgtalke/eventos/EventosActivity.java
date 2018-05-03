3b7b5f830e400d46b76c41fc0b5282fefaa74663
cristiano_gaspar <cristiano_gaspar@terceiros.sicredi.com.br>
2018-05-02 22:40:55 -0300

    Activity eventos

---
+++ app/src/main/java/garagetalke/com/appgtalke/eventos/EventosActivity.java
@@ -0,0 +1,15 @@
+package garagetalke.com.appgtalke.eventos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import garagetalke.com.appgtalke.R;
+
+public class EventosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventos);
+    }
+}
